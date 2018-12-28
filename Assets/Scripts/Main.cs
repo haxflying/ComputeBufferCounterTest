f5e6a6c7122d4fb4788e96d2b3bb10190074fad0
LitLov3 <912778422@qq.com>
2018-12-28 18:35:52 +0800

    init

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public ComputeShader cs;
+    public ComputeBuffer buffer;
+    public Shader outputShader;
+
+    private Material outputMat;
+    private int kernel;
+    private int pixelCount;
+    private void Start()
+    {
+        outputMat = new Material(outputShader);
+        pixelCount = Screen.width * Screen.height;
+        float[] data = new float[pixelCount];
+        data.Initialize();
+        buffer = new ComputeBuffer(pixelCount, sizeof(float), ComputeBufferType.Counter);
+        buffer.SetData(data);
+        kernel = cs.FindKernel("CSMain");
+        cs.SetBuffer(kernel, "res", buffer);
+        cs.SetInt("width", Screen.width);
+        cs.SetInt("height", Screen.height);
+        outputMat.SetBuffer("res", buffer);
+    }
+
+    private void OnPreRender()
+    {
+        cs.Dispatch(kernel, Screen.width / 8, Screen.height / 8, 1);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if(outputMat != null)
+        {
+            Graphics.Blit(src, dst, outputMat);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
+}
